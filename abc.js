3c3e79ccea9e1682110278c8acf317908bbe8d56
bansalinbox <bansalinbox@users.noreply.github.com>
2025-09-03 23:30:29 -0500

    Create abc.js

---
+++ abc.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class NwdSbaPage extends LightningElement {
+    disasterName = 'Hurricane Idalia';
+    applyBy = 'December 18, 2024';
+    documentsRequired = 3;
+
+    handleRegister() {
+        // Placeholder for registration logic
+        console.log('Register button clicked');
+    }
+}
